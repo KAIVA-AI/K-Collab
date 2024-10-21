ac57fe42d139d7c00fd34c22ca69a33731f55bcb
hao.nguyendang <hao.nguyendang@vietis.com.vn>
2024-10-21 11:34:42 +0700

    only select code block

--- packages/webview/src/pages/chat/components/chat-message-item.tsx
+++ packages/webview/src/pages/chat/components/chat-message-item.tsx
@@ -1,14 +1,35 @@
 import { Observer } from 'mobx-react';
 import { IMessage } from '../../../models';
 import { useRootStore } from '../../../stores';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 export const ChatMessageItem = (props: { message: IMessage }) => {
-  const { chatViewModel } = useRootStore();
   const { message } = props;
+  const { chatViewModel } = useRootStore();
+  const renderedMarkdownRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     // TODO render action buttons depend on div.codehilite tag
+    const codeBlockList =
+      renderedMarkdownRef.current?.querySelectorAll('div.codehilite');
+    if (codeBlockList) {
+      codeBlockList.forEach(codehilite => {
+        const preCode = codehilite.querySelector('pre');
+        const codeAction = document.createElement('div');
+        codeAction.className = 'message-code-action';
+        const insertIcon = document.createElement('i');
+        insertIcon.className = 'c-pointer codicon codicon-insert';
+        insertIcon.onclick = () =>
+          chatViewModel.clickInsertMessage(preCode?.innerHTML || '');
+        codeAction.appendChild(insertIcon);
+        const copyIcon = document.createElement('i');
+        copyIcon.className = 'c-pointer codicon codicon-copy';
+        copyIcon.onclick = () =>
+          chatViewModel.clickCopyMessage(preCode?.innerHTML || '');
+        codeAction.appendChild(copyIcon);
+        codehilite.prepend(codeAction);
+      });
+    }
   }, [message]);
 
   return (
@@ -24,7 +45,7 @@ export const ChatMessageItem = (props: { message: IMessage }) => {
             <div>{message.sender_full_name}</div>
           </div>
           <div className="message-content">
-            <div className="message-code-action">
+            {/* <div className="message-code-action">
               <i
                 className="c-pointer codicon codicon-insert"
                 onClick={() =>
@@ -35,8 +56,9 @@ export const ChatMessageItem = (props: { message: IMessage }) => {
                 className="c-pointer codicon codicon-copy"
                 onClick={() => chatViewModel.clickCopyMessage(message.content)}
               />
-            </div>
+            </div> */}
             <div
+              ref={renderedMarkdownRef}
               dangerouslySetInnerHTML={{ __html: message.content }}
               className="rendered_markdown"
             ></div>
