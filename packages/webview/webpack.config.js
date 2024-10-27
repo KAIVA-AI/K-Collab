f892631ac5c42f8ddd6a7b20fb00314c8ccaa701
hao.nguyendang <hao.nguyendang@vietis.com.vn>
2024-10-27 12:08:26 +0700

    update webpack

--- packages/webview/webpack.config.js
+++ packages/webview/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 require('dotenv').config();
-const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const BundleAnalyzerPlugin =
   require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
@@ -76,7 +75,6 @@ module.exports = {
     ],
   },
   plugins: [
-    new NodePolyfillPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
     }),
